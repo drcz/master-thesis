c7b2e3210f7df86818074c63c9374d17e085b995
dercz <drcz@o2.pl>
2017-11-25 07:42:29 +0100

    todo check if cpsizer can be extended to process closures

--- programs/testing-cpsing.scm
+++ programs/testing-cpsing.scm
@@ -52,3 +52,31 @@
                      (! (pow 2 3))))]
 
 ;;; (...)
+(define program-with-closure
+  '((define mk-adder (lambda (x) (lambda (y) (+ x y))))
+    (define app (lambda (f x) (f x))) ;; without it cpsizer breaks
+    (app (mk-adder 2) 3)))
+
+[e.g. (result program-with-closure) ===> 5]
+
+;;; not good TODO
+[e.g. (passing-program evil-closures)
+      ===>
+      ((define pass-mk-adder (lambda (x return)
+                               (return (lambda (y return)
+                                         (pass+ x y return)))))
+       (define pass-app
+         (lambda (f x return) (pass-f x return)))
+       (pass-mk-adder 2
+                      (lambda (mk-adder/2/18)
+                        (pass-app mk-adder/2/18 3 exit))))]
+
+; (...)
+
+[e.g.
+ (passing-program (passing-program '((define sq (lambda (x) (* x x))) (sq 3))))
+ ===>
+ ((define pass-pass-sq
+    (lambda (x return return)
+      (pass-pass* x x return return)))
+  (pass-pass-sq 3 exit exit))]
